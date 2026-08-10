9c739942f3e64319ea5fd41f1f413a1590b37b16
Feri <hosseinbigdeli335@gmail.com>
2026-08-10 21:51:49 +0330

    refactor: convert the function to the class file sorter

--- file_sorter.py
+++ file_sorter.py
@@ -2,33 +2,38 @@ import os
 import shutil
 
 
-def organize_photos():
-    downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
-    photo_dir = os.path.join(downloads_dir, "photo")
+class FileSorter:
+    def __init__(self):
+        self.downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
+        self.photo_dir = os.path.join(self.downloads_dir, "photo")
+        self.photo_formats = (".png", ".jpg", ".jpeg", ".webp", ".gif", ".bmp", ".jfif")
 
-    if not os.path.exists(photo_dir):
-        os.makedirs(photo_dir)
-        print("new folder name photo create successfully!")
-    else:
-        print("this folder is already exist!")
+    def organize_photos(self):
 
-    photo_formats = (".png", ".jpg", ".jpeg", ".webp", ".gif", ".bmp", ".jfif")
-    moved_count = 0
+        if not os.path.exists(self.photo_dir):
+            os.makedirs(self.photo_dir)
+            print("new folder name photo create successfully!")
+        else:
+            print("this folder is already exist!")
 
-    for file in os.listdir(downloads_dir):
-        full_path = os.path.join(downloads_dir, file)
+        moved_count = 0
 
-        if os.path.isfile(full_path) and file.lower().endswith(photo_formats):
-            try:
-                shutil.move(full_path, photo_dir)
-                moved_count += 1
-                print(f"Moved: {file} -> photo/")
-            except Exception as e:
-                print(f"Error moving {file}: {e}")
+        for file in os.listdir(self.downloads_dir):
+            full_path = os.path.join(self.downloads_dir, file)
 
-    print("-" * 30)
-    print(f"Task completed! Total photos moved: {moved_count}")
-    return moved_count
+            if os.path.isfile(full_path) and file.lower().endswith(self.photo_formats):
+                try:
+                    shutil.move(full_path, self.photo_dir)
+                    moved_count += 1
+                    print(f"Moved: {file} -> photo/")
+                except Exception as e:
+                    print(f"Error moving {file}: {e}")
+
+        print("-" * 30)
+        print(f"Task completed! Total photos moved: {moved_count}")
+        return moved_count
 
 
-organize_photos()
+if __name__ == "__main__":
+    sorter = FileSorter()
+    sorter.organize_photos()
